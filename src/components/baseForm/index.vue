aa4f862eab5c8a293a6cdf1501277b07932b997e
lifd <654321lfd>
2021-10-12 12:03:28 +0800

    完善基础表单组件

--- src/components/baseForm/index.vue
+++ src/components/baseForm/index.vue
@@ -3,25 +3,32 @@
         <el-form :model="form" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
             <el-form-item :class="{'isInline':item.isInline}"
                           v-for="(item, index) in list"
+                          v-if="visible[item.field]"
                           :prop="item.field"
                           :key="index">
                 <template slot="label">
-                      <span>
-                        {{item.label}}
-                      </span>
+                    <span>{{item.label}}</span>
                 </template>
                 <!--输入框-->
-                <el-input v-if="item.type==='input'||!item.type"
+                <el-input v-if="item.type==='input'"
                           v-model="form[item.field]"
                           :clearable="item.clearable||true"
-                          :disabled="item.disabled||disabled"
+                          :disabled="disable[item.field]||item.disabled||disabled"
+                          :style="{width:item.width||'400px'}"></el-input>
+                <!--文本域-->
+                <el-input v-if="item.type==='textarea'"
+                          v-model="form[item.field]"
+                          :clearable="item.clearable||true"
+                          :disabled="disable[item.field]||item.disabled||disabled"
+                          type="textarea"
+                          :autosize="{ minRows: item.minRows||2, maxRows: item.maxRows||4}"
                           :style="{width:item.width||'400px'}"></el-input>
                 <!--下拉框-->
                 <el-select v-if="item.type==='select'"
                            v-model="form[item.field]"
                            :clearable="item.clearable||true"
                            :multiple="item.multiple||false"
-                           :disabled="item.disabled||disabled"
+                           :disabled="disable[item.field]||item.disabled||disabled"
                            :style="{width:item.width||'400px'}"
                            placeholder="请选择">
                     <el-option v-for="i in item.list||[]"
@@ -33,17 +40,41 @@
                 <!--单选框-->
                 <el-radio-group v-if="item.type==='radio'"
                                 v-model="form[item.field]"
-                                :disabled="item.disabled||disabled">
+                                :disabled="disable[item.field]||item.disabled||disabled">
                     <el-radio v-for="i in item.list||[]"
                               :key="i.value"
                               :label="i.value">
                         {{i.label}}
                     </el-radio>
                 </el-radio-group>
+                <!--多选框-->
+                <el-checkbox-group v-if="item.type==='check'"
+                                   v-model="form[item.field]"
+                                   :disabled="disable[item.field]||item.disabled||disabled">
+                    <el-checkbox v-for="i in item.list||[]"
+                                 :key="i.value"
+                                 :label="i.value">
+                        {{i.label}}
+                    </el-checkbox>
+                </el-checkbox-group>
+                <!--插槽-->
+                <slot v-if="item.isSlot"
+                      :row="item"
+                      :form="form"
+                      :name="item.field">
+                </slot>
             </el-form-item>
-            <!--提交按钮-->
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+
+            <!--按钮-->
+            <el-form-item v-if="isButton">
+                <el-button @click="groupBtnFn('ruleForm',item,item.handler)"
+                           v-for="(item,index) in groupBtn"
+                           :key="index"
+                           size="mini"
+                           :type="item.type">
+                    {{item.label}}
+                </el-button>
+                <!--<el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>-->
             </el-form-item>
         </el-form>
     </div>
@@ -57,7 +88,7 @@
       * formList，表格参数
       * field：必传，字段名；
       * label：必传，名称；
-      * type：表单类型，默认是输入框；输入框-input,下拉选择框-select，单选框-radio；
+      * type：表单类型，输入框-input,下拉选择框-select，单选框-radio；
       * initVal：初始值,可是String、Number、Array、Boolean类型，初始值类型根据type来决定；
       * disabled:表单组件是否禁用；
       * clearable：是否显示清除按钮,默认显示；
@@ -69,6 +100,23 @@
       formList: {
         type: Array,
         required: true
+      },
+      isButton: {//是否显示按钮
+        type: Boolean,
+        default: true
+      },
+      ruleForm: Object,
+      groupBtn: {
+        type: Array,
+        default: function() {
+          return [
+            {
+              type: "primary",
+              label: "提交"
+            }
+          ];
+
+        }
       }
     },
     data() {
@@ -76,7 +124,9 @@
         disabled: false,
         form: {},
         rules: {},
-        list: []
+        list: [],
+        visible: {},
+        disable: {}
       };
     },
     watch: {
@@ -86,41 +136,67 @@
         handler(val) {
           console.log(1111111);
           console.log(val);
+          if (!val || val.length < 1) {
+            return false;
+          }
+          this.rules = {};
+          this.form = {};
           const data = window.utils.deepClone(val);
           this.list = data;
           data.forEach((item, index) => {
             //设置表单值
             if (item.initVal) {
-              console.log("aaaaaa");
-              console.log(item.field);
               this.$set(this.form, item.field, item.initVal);
             } else {
               this.$set(this.form, item.field, "");
-              console.log("bbbbbbbb");
-              console.log(item.field);
             }
-            //设置校验
+            //设置校验规则
             this.$set(this.rules, item.field, item.rules || []);
-          });
 
+            //设置是否可见、可读
+            this.$set(this.visible, item.field, true);
+            this.$set(this.disable, item.field, false);
+          });
+        }
+      },
+      ruleForm: {
+        deep: true,
+        immediate: true,
+        handler(val) {
+          console.log(222222222);
+          console.log(val);
+          if (val) {
+            this.form = val;
+          }
         }
       }
     },
+    created() {
+
+    },
     methods: {
-      submitForm(formName) {
-        console.log("提交了1111111");
-        console.log(this.form);
+      groupBtnFn(formName, item, callback) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
+            if (callback) {
+              callback && callback(item,this.$refs[formName]);
+            } else {
+              this.$emit("formData", Object.assign({}, this.form));
+            }
           } else {
+            this.$emit("formData", null);
             console.log("error submit!!");
             return false;
           }
         });
+
+      },
+      submitForm(formName) {
       },
       resetForm(formName) {
         this.$refs[formName].resetFields();
       }
+
     }
   };
 </script>
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,6 +1,33 @@
 <template>
     <div>
-        <BaseForm :formList="formList"></BaseForm>
+        <h4>有初始值</h4>
+        <BaseForm ref="baseform1"
+                  :formList="formList"
+                  :ruleForm="form1"
+                  :groupBtn="groupBtn1"
+                  @formData="setFormData1">
+            <template slot="slotCheck" slot-scope="scope">
+                <el-checkbox-group v-model="scope.form.slotCheck">
+                    <el-checkbox label="复选框 A"></el-checkbox>
+                    <el-checkbox label="复选框 B"></el-checkbox>
+                    <el-checkbox label="复选框 C"></el-checkbox>
+                </el-checkbox-group>
+            </template>
+        </BaseForm>
+
+        <h4>无初始值</h4>
+        <BaseForm ref="baseform2"
+                  :formList="formList"
+                  :ruleForm="form2"
+                  @formData="setFormData2">
+            <template slot="slotCheck" slot-scope="scope">
+                <el-checkbox-group v-model="scope.form.slotCheck">
+                    <el-checkbox label="复选框 A"></el-checkbox>
+                    <el-checkbox label="复选框 B"></el-checkbox>
+                    <el-checkbox label="复选框 C"></el-checkbox>
+                </el-checkbox-group>
+            </template>
+        </BaseForm>
     </div>
 </template>
 <script>
@@ -27,7 +54,7 @@
             label: "禁用表单",
             type: "input",
             initVal: "测试项目",
-            disabled: true,
+            disabled: true,//设置禁用
             rules: [{
               required: true, message: "请填写此项", trigger: "blur"
             }]
@@ -85,45 +112,169 @@
             rules: [{
               required: true, message: "请选择", trigger: "change"
             }]
+          },
+          {
+            field: "check1",
+            label: "多选框1",
+            type: "check",
+            initVal: [],
+            list: [
+              {
+                label: "复选框 A",
+                value: 1
+              }, {
+                label: "复选框 B",
+                value: 2
+              }, {
+                label: "复选框 C",
+                value: 3
+              }
+            ],
+            rules: [{
+              required: true, message: "请选择", trigger: "change"
+            }]
+          },
+          {
+            field: "check2",
+            label: "多选框2",
+            type: "check",
+            initVal: [],
+            list: [
+              {
+                label: "复选框 A",
+                value: 1
+              }, {
+                label: "复选框 B",
+                value: 2
+              }, {
+                label: "复选框 C",
+                value: 3
+              }
+            ],
+            rules: [{
+              required: true, message: "请选择", trigger: "change"
+            }]
+          },
+          {
+            field: "slotCheck",
+            label: "我是插槽",
+            isSlot: true,//是插槽则必传
+            initVal: [],
+            rules: [{
+              required: true, message: "请选择", trigger: "change"
+            }]
+          },
+          {
+            field: "textarea",
+            label: "文本域",
+            type: "textarea",
+            initVal: null,
+            minRows: 4,//设置文本域最小行数
+            maxRows: 8,//设置文本域最大行数
+            rules: [{
+              required: true, message: "请输入", trigger: "blur"
+            }]
           }
         ],
-        ruleForm: {
-          name: "",
-          region: "",
-          date1: "",
-          date2: "",
-          delivery: false,
-          type: [],
-          resource: "",
-          desc: ""
+        form1: {
+          name: "测试项目",
+          disabled: "测试禁用",
+          select1: 1,
+          select2: [1],
+          radio: 1,
+          check1: [1],
+          check2: [1, 2],
+          slotCheck: ["复选框 C"],
+          textarea: "test"
         },
-        rules: {
-          name: [
-            { required: true, message: "请输入活动名称", trigger: "blur" },
-            { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-          ],
-          region: [
-            { required: true, message: "请选择活动区域", trigger: "change" }
-          ],
-          date1: [
-            { type: "date", required: true, message: "请选择日期", trigger: "change" }
-          ],
-          date2: [
-            { type: "date", required: true, message: "请选择时间", trigger: "change" }
-          ],
-          type: [
-            { type: "array", required: true, message: "请至少选择一个活动性质", trigger: "change" }
-          ],
-          resource: [
-            { required: true, message: "请选择活动资源", trigger: "change" }
-          ],
-          desc: [
-            { required: true, message: "请填写活动形式", trigger: "blur" }
-          ]
-        }
+        form2: null,
+        groupBtn1: [
+          {
+            type: 'primary',
+            btnType:'edit',
+            label: '编辑',
+            handler: this.edit
+          }
+        ],
       };
     },
+    created() {
+      this.$nextTick(() => {
+        if (this.form1.radio === 1) {
+          this.$refs.baseform1.visible.check2 = false;
+          this.$refs.baseform1.disable.check1 = true;
+          // this.$set(this.formList[6],'visible',true)
+        } else {
+          this.$refs.baseform1.visible.check2 = true;
+          this.$refs.baseform1.disable.check1 = false;
+          // this.$set(this.formList[6],'visible',false)
+        }
+      });
+    },
+    watch: {
+      form1: {
+        deep: true,
+        handler(val) {
+          console.log("dddddddddddddddddd");
+          if (val.radio === 1) {
+            this.$refs.baseform1.visible.check2 = false;
+            this.$refs.baseform1.disable.check1 = true;
+          } else {
+            this.$refs.baseform1.visible.check2 = true;
+            this.$refs.baseform1.disable.check1 = false;
+          }
+        }
+      }
+    },
     methods: {
+      setFormData1(val) {
+        if (val) {
+          console.log("bbbbbbbbbb");
+          console.log(this.form1);
+          this.submit();
+        }
+      },
+      setFormData2(val) {
+        if (val) {
+          console.log("ccccccccccc");
+          console.log(this.form1);
+          this.submit();
+        }
+      },
+      submit() {
+        this.$message.success("提交成功");
+      },
+      edit(item){
+        console.log(88888888888)
+        console.log(item)
+        this.groupBtn1=[
+          {
+            type: 'primary',
+            btnType:'submit',
+            label: '提交',
+          },
+          {
+            type: '',
+            btnType:'cancel',
+            label: '取消',
+            handler: this.cancel
+          }
+        ]
+      },
+      cancel(){
+        this.groupBtn1=[
+          {
+            type: 'primary',
+            btnType:'edit',
+            label: '编辑',
+            handler: this.edit
+          }
+        ]
+      },
+      getFormData(){
+        console.log('eeeeeeeeeeeeeeeee')
+      }
+
     }
   };
 </script>
