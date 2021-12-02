9eb2341f0e881acd53cd0064727457f166d98dd7
lifangdan <845154579@qq.com>
2021-12-02 17:30:20 +0800

    添加echart地图

--- src/views/echart/components/map.vue
+++ src/views/echart/components/map.vue
@@ -1,13 +1,138 @@
 <template>
-
+    <div ref="echart" id="map"></div>
 </template>
 
 <script>
+  import * as echarts from "echarts";
+  import GuangDong from "echarts/map/json/province/guangdong";
+
   export default {
-    name: "map"
+    data() {
+      return {
+        myChart: null
+      };
+    },
+    created() {
+      this.$nextTick(() => {
+        this.drawMap();
+      });
+
+    },
+    methods: {
+      drawMap() {
+        if (this.myChart) {
+          this.myChart.dispose();
+        }
+        this.myChart = echarts.init(this.$refs.echart);
+        echarts.registerMap("广东", GuangDong);
+        this.optionXyMap01 = {
+          timeline: {
+            show: false
+          },
+          baseOption: {
+            animation: true,
+            animationDuration: 1000,
+            animationEasing: "cubicInOut",
+            animationDurationUpdate: 1000,
+            animationEasingUpdate: "cubicInOut",
+            tooltip: {
+              trigger: "item", // hover触发器
+              confine: true,
+              formatter: (e) => {
+              }
+            },
+            geo: {
+              show: true,
+              map: "广东",
+              roam: true, //是否允许缩放
+              zoom: 1,
+              label: {
+                emphasis: {
+                  show: true
+                }
+              },
+              itemStyle: {
+                normal: {
+                  borderColor: "#646a70",
+                  borderWidth: 1,
+                  // 地区的颜色
+                  areaColor: {
+                    type: "radial",
+                    x: 0.5,
+                    y: 0.5,
+                    r: 0.8,
+                    colorStops: [{
+                      offset: 0,
+                      color: "rgba(147, 235, 248, 0)" // 0% 处的颜色
+                    }, {
+                      offset: 1,
+                      color: "rgba(147, 235, 248, .2)" // 100% 处的颜色
+                    }],
+                    globalCoord: false // 缺省为 false
+                  },
+                  shadowColor: "#00bc9d",//地图边框颜色
+                  shadowOffsetX: -1,
+                  shadowOffsetY: 1,
+                  shadowBlur: 1
+                },
+                emphasis: {
+                  areaColor: "#00bc9d",//鼠标经过时区域的颜色
+                  borderWidth: 0
+                }
+              }
+            }
+          },
+          options: [{
+            series: [
+              //地图点的动画效果
+              {
+                type: "effectScatter",
+                coordinateSystem: "geo",
+                data: [{
+                  name: "广州",
+                  value: [113.280637, 23.125178, 90]//前面是坐标，后面是值,
+                }],
+
+                symbolSize: (val) => {
+                  return val[2] / 10;
+                },
+                rippleEffect: {//波纹绘制方式
+                  brushType: "stroke"
+                },
+                hoverAnimation: true,
+                label: {
+                  normal: {
+                    formatter: "{b}",
+                    position: "right",
+                    show: false // 是否显示公司名称
+                  }
+                },
+                itemStyle: {
+                  normal: {
+                    // 地图点的颜色
+                    color: "#ffa022"
+                  }
+                },
+                animation: true,
+                zlevel: 2
+              }
+            ]
+          }]
+        };
+        this.myChart.setOption(this.optionXyMap01);
+        this.myChart.off("click");
+        // this.myChart.on('click', this.clickArea);//点击事件
+      }
+    }
   };
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .echartBox {
+        #map {
+            height: 500px;
+            width: 100%;
+            background-color: #26404b;
+        }
+    }
 </style>
